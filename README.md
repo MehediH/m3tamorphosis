9ec6221d7615771a65471cb66673689fc5001d82
Mehedi <mehedi.dip@outlook.com>
2021-04-04 18:35:47 +0100

    Fixes

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # m3tamorphosis 
 
-Experimenting with Next.js, Three.js, and the Spotify API :)
+m3tamorphosis is a 3D Spotify player built with Next.js, Three.js, and the Spotify Web Playback SDK. 
+
+[You can try m3tamorphosis here.](m3tamorphosis.vercel.app)
+
+Aaaand here's a quick demo:
--- components/PlaybackControls.js
+++ components/PlaybackControls.js
@@ -16,7 +16,7 @@ export default function PlaybackControls({ paused, textColor, disallows }){
     }
 
     return (
-        <Html transform position={[-1.2, -4.5, 0]} className={`${styles.playbackControls} ${ textColor === "#000" ? styles.darkIcons : '' }`}>
+        <Html position={[-1.2, -4.5, 0]} className={`${styles.playbackControls} ${ textColor === "#000" ? styles.darkIcons : '' }`}>
             <FiSkipBack onClick={() => handleAction("prev")} className={disallows?.skipping_prev ? styles.disabled : ''} disabled={disallows?.skipping_prev}/>
             { !paused ? <FiPause onClick={handleAction}/> : <FiPlay onClick={handleAction}/> }
             <FiSkipForward onClick={() => handleAction("next")} className={disallows?.skipping_next ? styles.disabled : ''} disabled={disallows?.skipping_next}/>
